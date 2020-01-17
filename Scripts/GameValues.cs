c9e641b2529e6d45a6ec05e4a0a8ce30d0533014
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-17 17:21:20 -0500

    added documentation

--- Scripts/GameValues.cs
+++ Scripts/GameValues.cs
@@ -6,18 +6,40 @@ namespace PersistentData
 {
     public static class GameValues
     {
+        /// <summary>
+        /// Save some data, identified with a key
+        /// </summary>
+        /// <typeparam name="T">The type of data to save</typeparam>
+        /// <param name="key">The key to identify the data with</param>
+        /// <param name="value">The data to save</param>
         public static void Set<T>(string key, T value) 
             => SerializationLogic.Set(key, value);
 
+        /// <summary>
+        /// Load some data, identified by key
+        /// </summary>
+        /// <typeparam name="T">The type to load the data as</typeparam>
+        /// <param name="key">The key the data is identified with</param>
+        /// <param name="defaultValue">If there was no data saved with the given key, save and return this</param>
+        /// <returns>The loaded value</returns>
         public static T Get<T>(string key, T defaultValue) 
             => SerializationLogic.Get(key, defaultValue);
 
+        /// <summary>
+        /// Check if saved data exists identified by this key
+        /// </summary>
         public static bool KeyExists(string key)
             => SerializationLogic.KeyExists(key);
 
+        /// <summary>
+        /// Delete the data saved at this key, if there is any
+        /// </summary>
         public static void DeleteKey(string key)
             => SerializationLogic.DeleteKey(key);
 
+        /// <summary>
+        /// Delete all the saved data in GameValues. Be very very careful with this!
+        /// </summary>
         public static void DeleteAll(string key)
             => SerializationLogic.DeleteAll();
     }
