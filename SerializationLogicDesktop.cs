e944f9bbc8456b0897c2f308ebf72c1d48ffdd47
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-17 00:11:28 -0500

    add classes

---
+++ SerializationLogicDesktop.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR || UNITY_STANDALONE
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SUCC;
+
+namespace PersistentData
+{
+    internal static class SerializationLogicDesktop
+    {
+        internal static void Set<T>(string key, T value)
+        {
+
+        }
+
+        internal static T Get<T>(string key)
+        {
+
+        }
+    }
+
+}
+#endif
