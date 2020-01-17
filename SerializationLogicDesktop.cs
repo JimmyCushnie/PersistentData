0b1d39259c1f11bdc0a779f8adad525b3558b7c3
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-17 15:45:46 -0500

    added desktop serialization logic

--- SerializationLogicDesktop.cs
+++ SerializationLogicDesktop.cs
@@ -9,14 +9,16 @@ namespace PersistentData
 {
     internal static class SerializationLogicDesktop
     {
+        static readonly DataFile File = new DataFile(nameof(PersistentData));
+
         internal static void Set<T>(string key, T value)
         {
-
+            File.Set(key, value);
         }
 
         internal static T Get<T>(string key)
         {
-
+            return File.Get<T>(key);
         }
     }
 
