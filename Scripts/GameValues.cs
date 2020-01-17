37a54dc702d4310e23f8a31a84c45fde752dad55
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-17 17:16:12 -0500

    coded it all

---
+++ Scripts/GameValues.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PersistentData
+{
+    public static class GameValues
+    {
+        public static void Set<T>(string key, T value) 
+            => SerializationLogic.Set(key, value);
+
+        public static T Get<T>(string key, T defaultValue) 
+            => SerializationLogic.Get(key, defaultValue);
+
+        public static bool KeyExists(string key)
+            => SerializationLogic.KeyExists(key);
+
+        public static void DeleteKey(string key)
+            => SerializationLogic.DeleteKey(key);
+
+        public static void DeleteAll(string key)
+            => SerializationLogic.DeleteAll();
+    }
+}
---
+++ Scripts/SerializationLogic.cs
@@ -0,0 +1,71 @@
+﻿#if UNITY_EDITOR || UNITY_STANDALONE
+
+using SUCC;
+
+namespace PersistentData
+{
+    internal static class SerializationLogic
+    {
+        static DataFile File = new DataFile(nameof(GameValues));
+
+        internal static void Set<T>(string key, T value) 
+            => File.Set(key, value);
+
+        internal static T Get<T>(string key, T defaultValue) 
+            => File.Get(key, defaultValue);
+
+        internal static bool KeyExists(string key)
+            => File.KeyExists(key);
+
+        internal static void DeleteKey(string key)
+            => File.DeleteKey(key);
+
+        internal static void DeleteAll()
+        {
+            System.IO.File.Delete(File.FilePath);
+            File = new DataFile(nameof(GameValues));
+        }
+    }
+}
+
+#else
+
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace PersistentData
+{
+    internal static class SerializationLogic
+    {
+        internal static void Set<T>(string key, T value)
+        {
+            string json = JsonConvert.SerializeObject(value);
+            PlayerPrefs.SetString(key, json);
+        }
+
+        internal static T Get<T>(string key, T defaultValue)
+        {
+            if (KeyExists(key))
+            {
+                string json = PlayerPrefs.GetString(key);
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            else
+            {
+                Set(key, defaultValue);
+                return defaultValue;
+            }
+        }
+
+        internal static bool KeyExists(string key)
+            => PlayerPrefs.HasKey(key);
+
+        internal static void DeleteKey(string key)
+            => PlayerPrefs.DeleteKey(key);
+
+        internal static void DeleteAll()
+            => PlayerPrefs.DeleteAll();
+    }
+}
+
+#endif
